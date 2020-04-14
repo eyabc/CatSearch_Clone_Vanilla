1797c986d0b62b0ffe224d940b51f86314520204
eyabc <bey4314@naver.com>
2020-04-14 15:30:14 +0900

    Search Result DOM

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,3 +1,8 @@
 export default class SearchResult {
-
+  constructor({ $target }) {
+    const $searchResult = document.createElement('div');
+    this.$searchResult = $searchResult;
+    this.$searchResult.className = "SearchResult";
+    $target.appendChild(this.$searchResult);
+  }
 }
