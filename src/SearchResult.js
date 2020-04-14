91e0d8de08a840ef76f75b81b605a98a0d6b9c28
eyabc <bey4314@naver.com>
2020-04-15 01:33:24 +0900

    Lazy Load

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,18 +1,29 @@
 export default class SearchResult {
   constructor({ $target, catData, openInfo }) {
+    this.lazyLoading = () => {
+      const imgs = document.querySelectorAll('.item img[data-src]');
+      const start = window.scrollY;
+      const end = start + window.outerHeight;
+      const onImages = [ ...imgs ].filter(v =>  start <= v.offsetTop && v.offsetTop <= end);
+      onImages.forEach(v => {
+        v.src = v.dataset.src;
+        v.parentElement.style = '';
+      })
+    };
     const $searchResult = document.createElement('div');
     this.$searchResult = $searchResult;
     this.$searchResult.className = "SearchResult";
     $target.appendChild(this.$searchResult);
 
     this.data = catData;
-    this.openInfo = openInfo
+    this.openInfo = openInfo;
+    window.addEventListener('load', this.lazyLoading);
+    window.addEventListener('scroll', this.lazyLoading);
   }
   setState(nextData) {
     this.data = nextData;
     this.render();
   }
-
   render() {
     if(this.data === false) {
       this.$searchResult.innerHTML = '정상적인 요청이 아닙니다.';
@@ -20,15 +31,19 @@ export default class SearchResult {
     }
     this.$searchResult.innerHTML = this.data.length
       ? this.data.map(cat => `
-        <article class='item'>
-            <img src=${cat.url} alt=${(cat.breeds.length && cat.breeds[0].name)|| cat.id}>
+        <article class="item" style="height: 200px;">
+            <img data-src="${cat.url}" alt="${(cat.breeds.length && cat.breeds[0].name)|| cat.id}" />
         </article>
       `).join("")
       : '검색 결과가 없습니다.';
 
+    this.lazyLoading();
+
     this.$searchResult.querySelectorAll('.item')
       .forEach(($item, index) => {
-        $item.addEventListener('click', () => {
+        $item.addEventListener('click', e => {
+          e.preventDefault();
+          e.stopPropagation();
           this.openInfo(this.data[index].id);
         })
     })
